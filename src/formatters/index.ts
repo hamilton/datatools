3784a1618efa9d441905644b6c41fcbeae0d5c18
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-12-12 20:55:37 -0800

    update comment for justEnoughPrecision

--- src/formatters/index.ts
+++ src/formatters/index.ts
@@ -1,4 +1,9 @@
-/** This heuristic is courtesy Dominik Moritz */
+/** This heuristic is courtesy Dominik Moritz:
+ * “I wanted integers to be shown completely (because we already
+ * use the digits so might as well) and show floats
+ * such that the format gives you enough resolution
+ * to distinguish numbers when they should be distinguishable.”
+ */
 export function justEnoughPrecision(n: number) {
   if (typeof n !== "number") throw Error("argument must be a number");
   const str = n.toString();
